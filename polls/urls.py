6d173cb41655988b186f297de97f3a571fce124e
Neo Nguyen <neonguyen.ca@gmail.com>
2023-10-03 22:46:29 -0600

    Template part 1

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about', views.aboutPage, name='about'),
+    # path('about', views.aboutPage, name='about'),
     path('', views.index, name="index"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello world')
+    myName = "Neo"
+    property = ["phone","laptop","home"]
+    context={"name":myName,"property":property}
+    return render(request, "polls/index.html", context)
 
-def aboutPage(request):
-    return HttpResponse('<h1>This is about page</h1>')
+# def aboutPage(request):
+#     return HttpResponse('<h1>This is about page</h1>')
